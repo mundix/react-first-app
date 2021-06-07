5e6b4911046706aa34607772307f13ace432e00f
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-06-07 00:14:39 -0400

    Add new Task to task list

--- src/App.js
+++ src/App.js
@@ -9,16 +9,31 @@ import tasks from './sample/task.json';
 import Tasks from './components/Tasks';
 import TaskForm from './components/TaskForm';
 
+//Ahora vamos a comunicar componentes para agregar state en App desde TaskForm 
 
 class App extends Component {
 
   state = {
     tasks: tasks
   }
+  //quiero pasarle esta funcion a task form , se puede pasar atraves de una props 
+  addTask = (title, description) => {
+    // console.log(title, description);
+    const newTask = {
+      title: title,
+      description: description,
+      id: this.state.tasks.length
+    }
+    console.log(newTask);
+    this.setState({
+      //ASi es como se asigna 
+      tasks: [...this.state.tasks, newTask]
+    })
+  }
 
   render() {
     return <div>
-      <TaskForm/>
+      <TaskForm addTask={this.addTask}/>
       <Tasks tasks={this.state.tasks} />
     </div>
   }
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,15 +1,18 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 
 export default class TaskForm extends Component {
-    
+
     state = {
         title: '',
         description: ''
     }
 
     onSubmitEvent = e => {
-        e.preventDefault();        
+        e.preventDefault();
         console.log(this.state);
+         //llamo desde los props  el metodo addTask() 
+        //  this.props.addTask('title1', 'description 1');
+         this.props.addTask(this.state.title, this.state.description);
     }
     onChangeEvent = e => {
         this.setState({
@@ -18,21 +21,23 @@ export default class TaskForm extends Component {
     }
 
     render() {
+       
+
         return (
             <form onSubmit={this.onSubmitEvent}>
-                <input type='text' 
-                name='title'
-                placeholder='Write a Task' 
-                onChange={this.onChangeEvent}
-                value={this.state.title}/>
-                <br/>
-                <br/>
+                <input type='text'
+                    name='title'
+                    placeholder='Write a Task'
+                    onChange={this.onChangeEvent}
+                    value={this.state.title} />
+                <br />
+                <br />
                 <textarea placeholder='escribe description'
-                name='description'
-                onChange={this.onChangeEvent}
-                value={this.state.description}
+                    name='description'
+                    onChange={this.onChangeEvent}
+                    value={this.state.description}
                 ></textarea>
-                <br/>
+                <br />
                 <button type='submit'>Save Task</button>
             </form>
         )
